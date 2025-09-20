ec23a3008f5e800b3ecae9cb6820b6eb28cf71f1
peterihimire <peterihimire@gmail.com>
2025-09-20 11:13:27 +0100

    The primitive java data types

--- JavaBasic.java
+++ JavaBasic.java
@@ -1,22 +1,35 @@
 
+// // Variables
+// public class JavaBasic
+// {
+//   public static void main(String[] args)
+//   {
+//     int num1 = 6; //this is a variable
+//     int num2 = 5;
+//     int num3 = 2; 
+//     int num4 = 9;
 
+//     int result = num1 + num2 * num4 + num3;
+//     System.out.println( result);
+//   }
+// }
+// // && || : These are called short-circuits in programming
+
+// The Primitive data types
 public class JavaBasic
 {
   public static void main(String[] args)
   {
-//    int x = 6;
-//    int y = 5;
-//    int a = 2;
-//    int b = 9;
-//
-//    boolean result = x > y && a < b;
-    System.out.println("Hello world");
+    int num = 5;
+    byte by = 127; 
+    short sh = 5945;
+    long l = 5945L;
 
-    // if(x > 10 && x < 2)
-    //   System.out.println("Hello");
-    // else
-    //   System.out.println("World");
+    float f = 6f; 
+    double d = 5.8;
+
+    boolean b = true;
+    
+    char chr = 'k';
   }
 }
-// && || : These are called short-circuits in programming
-
